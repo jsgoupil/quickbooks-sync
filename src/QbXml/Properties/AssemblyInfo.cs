7778ae2ac14c48312eba23bd33129515d0cd06d8
jsgoupil <jsgoupil@gmail.com>
2015-02-07 18:57:20 -0800

    Add .travis.yml. Modify the AssemblyInfo.cs

--- src/QbXml/Properties/AssemblyInfo.cs
+++ src/QbXml/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QuickbooksDesktopSync")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("QuickbooksSync - QbXml")]
+[assembly: AssemblyDescription("Create requests and parse responses from Quickbooks QbXml.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QuickbooksDesktopSync")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Jean-Sébastien Goupil")]
+[assembly: AssemblyProduct("QuickbooksSync")]
+[assembly: AssemblyCopyright("Copyright © Jean-Sébastien Goupil 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
