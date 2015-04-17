155a3c5fc89500eec5d4ffd643448cfe6a5b9c7c
jsgoupil <jsgoupil@gmail.com>
2015-04-17 16:35:09 -0700

    Adding properties to the QbResponse interface.

--- src/QbXml/Objects/QbResponse.cs
+++ src/QbXml/Objects/QbResponse.cs
@@ -2,5 +2,9 @@
 {
     public interface QbResponse
     {
+        string requestID { get; set; }
+        string statusCode { get; set; }
+        string statusMessage { get; set; }
+        string statusSeverity { get; set; }
     }
 }
