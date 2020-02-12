b664b763e8d97914b65e5d89b8df2e6e0b3c1036
Jean-Sébastien Goupil <contact@jsgoupil.com>
2020-02-12 11:06:38 -0800

    Update to version QbXml2.1.1

--- create-nuget.bat
+++ create-nuget.bat
@@ -5,11 +5,11 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 md .\publish
 
-dotnet pack .\src\QbXml\QbXml.csproj -c Release --include-symbols --no-build -o ..\..\publish
+dotnet pack .\src\QbXml\QbXml.csproj -c Release --include-symbols --no-build -o .\publish
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet pack .\src\WebConnector\WebConnector.csproj -c Release --include-symbols --no-build -o ..\..\publish
+dotnet pack .\src\WebConnector\WebConnector.csproj -c Release --include-symbols --no-build -o .\publish
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet pack .\src\WebConnector.AspNetCore\WebConnector.AspNetCore.csproj -c Release --include-symbols --no-build -o ..\..\publish
+dotnet pack .\src\WebConnector.AspNetCore\WebConnector.AspNetCore.csproj -c Release --include-symbols --no-build -o .\publish
 if %errorlevel% neq 0 exit /b %errorlevel%
