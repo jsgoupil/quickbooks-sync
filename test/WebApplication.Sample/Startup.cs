0e8a849adef3e9023c9184a3d9dfe965fd5449d6
Jean-Sébastien Goupil <contact@jsgoupil.com>
2020-11-16 09:48:57 -0800

    Updating to .NET Core 5 for samples/tests

--- test/WebApplication.Sample/Startup.cs
+++ test/WebApplication.Sample/Startup.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using WebApplication.Sample.Application;
 using WebApplication.Sample.Application.Steps;
 using WebApplication.Sample.Db;
@@ -40,9 +40,7 @@ namespace WebApplication.Sample
                     ;
                 });
 
-            services
-                .AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllersWithViews();
 
             services
                 .AddScoped<ApplicationDbContext>();
@@ -52,7 +50,7 @@ namespace WebApplication.Sample
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             // Indicates the path for your service to be reached.
             app
@@ -67,16 +65,20 @@ namespace WebApplication.Sample
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
